a917d8c25a3307284cbb25d8ac5d65fe2d28ac64
Alexandru Grigore <spider_alex01@yahoo.com>
2024-02-07 18:30:29 +0200

    Calculator-In-React

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Main from"./Main"
+
+function App() {
+  return<>
+<Main></Main>
+  </>
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,43 @@
+import "./style.css"
+import{useState}from "react"
+
+
+function Main() {
+  const[inputValue,setInputValue]=useState("");
+    
+function display(value){
+  setInputValue(inputValue+value)
+}
+function calculator(){
+  let answers=eval(inputValue)
+  setInputValue(answers)
+}
+function clear(){
+  setInputValue("")
+}
+return<>
+<form name="calc"className="calculator">
+<input type="text"className="value" value={inputValue}/>
+<span className="num clear" onClick={()=>clear()} >C</span>
+<span onClick={()=>display("/")}>/</span>
+<span onClick={()=>display("*")}>*</span>
+<span onClick={()=>display("7")}>7</span>
+<span onClick={()=>display("8")}>8</span>
+<span onClick={()=>display("9")}>9</span>
+<span onClick={()=>display("-")}>-</span>
+<span onClick={()=>display("4")}>4</span>
+<span onClick={()=>display("5")}>5</span>
+<span onClick={()=>display("6")}>6</span>
+<span className="plus" onClick={()=>display("+")}>+</span>
+<span onClick={()=>display("1")}>1</span>
+<span onClick={()=>display("2")}>2</span>
+<span onClick={()=>display("3")}>3</span>
+<span onClick={()=>display("0")}>0</span>
+<span onClick={()=>display("00")}>00</span>
+<span onClick={()=>display(".")}>.</span>
+<span className="num equel" onClick={()=>calculator()}>=</span>
+</form>
+</>
+}
+
+export default Main;
